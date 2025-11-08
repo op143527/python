1e3f3cfc22f5d7333e625889e97c8e96912d7952
op143527 <op143527@gmail.com>
2025-11-08 15:10:31 +0800

    新增 prj04.py：實作成績等級判斷，包含錯誤處理

---
+++ class4/prj04.py
@@ -0,0 +1,19 @@
+try:
+    m = float(input("請輸入成績:"))
+    A = 90
+    B = 80
+    C = 70
+    D = 60
+    E = 59
+    if m >= A:
+        print("你的成績等級為A")
+    elif m >= B:
+        print("你的成績等級為B")
+    elif m >= C:
+        print("你的成績等級為C")
+    elif m >= D:
+        print("你的成績等級為D")
+    elif m <= E:
+        print("你的成績等級為E")
+except:
+    print("輸入錯誤!")
